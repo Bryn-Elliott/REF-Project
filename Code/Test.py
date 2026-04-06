f755f1e4ebe2e65c5dc0458d38cd36c71c5e94fb
Bryn <u03be21@abdn.ac.uk>
2026-04-06 09:44:03 +0100

    AP done

--- Code/Test.py
+++ Code/Test.py
@@ -1,7 +1,7 @@
-import time, Main, ListGen, csv
+import time, Main, ListGen, csv, openpyxl
 
 def Test():
-    testInputVals = [[100, 250, 0.01], [1000, 250, 0.01], [100, 2500, 0.01], [100, 250, 0.1], [10, 250, 0.01], [100, 25, 0.01], [100, 250, 0.001]]
+    testInputVals = [[100, 250, 0.01], [200, 250, 0.01], [100, 500, 0.01], [100, 250, 0.1], [10, 250, 0.01], [100, 25, 0.01], [100, 250, 0.001]]
     testNames = ["Control", "High Pop", "High Gen", "High Mut", "Low Pop", "Low Gen", "Low Mut"]
     testResults = []
     ListGen.Generate([1590, 6])
@@ -15,22 +15,33 @@ def Test():
         timeDuration = time.time() - startTime
         testResults.append([name, inputVals, fitOverTime, greedyFit, timeDuration])
 
-    csv_filepath = "Data/Aglo Parameter Test Results.csv"
+    csvFilepath = "Data/Aglo Parameter Test Results.csv"
 
     # Define the field names (headers)
-    fieldnames = ["Name", "Input Values", "Fitness over Time", "Greedy Algorithm Fitness", "Duration of Run"]
+    generalData = []
+    generalData.append(["Name", "Input Values", "Final Fitness", "Greedy Fitness", "Duration of Run"])
 
-    data = []
-    data.append(fieldnames)
+    fitnessData = []
 
     for test in testResults:
         name, inputVals, fitOverTime, greedyFit, timeDuration = test
-        data.append([name, inputVals, fitOverTime, greedyFit, timeDuration])
+        generalData.append([name, inputVals, fitOverTime[-1][1], greedyFit, timeDuration])
+        fitnessData.append(fitOverTime)
 
     # Writing to CSV
-    with open(csv_filepath, mode='w', newline='') as file:
+    with open(csvFilepath, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows(data)
+        writer.writerows(generalData)
+
+    csvFilepaths = ["Data/AP/Control Test Results.csv","Data/AP/High Pop Test Results.csv","Data/AP/High Gen Test Results.csv","Data/AP/High Mut Test Results.csv","Data/AP/Low Pop Test Results.csv","Data/AP/Low Gen Test Results.csv","Data/AP/Low Mut Test Results.csv"]
+
+    for x in range(0, len(csvFilepaths) - 1):
+        with open(csvFilepaths[x], mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Generation", "Fitness"])
+            writer.writerows(fitnessData[x])
+        print(x)
+        print(len(testResults))
 
     testInputVals = [[1590, 6], [996, 6], [1590, 3], [3180, 6], [1590, 12]]
     testNames = ["Control", "Low Aca", "Low Ratio", "High Aca", "High Ratio"]
@@ -40,20 +51,33 @@ def Test():
         name = testNames[x]
         startTime = time.time()
         ListGen.Generate(inputVals)
-        output = Main.Run([100, 250, 0.01])
+        output = Main.Run([10, 25, 0.01])
         fitOverTime = output[0]
         greedyFit = output[1]
         timeDuration = time.time() - startTime
         testResults.append([name, inputVals, fitOverTime, greedyFit, timeDuration])
 
-    csv_filepath = "Data/Input Value Test Results.csv"
+    csvFilepath = "Data/Input Value Test Results.csv"
 
-    # Define the field names (headers)
-    fieldnames = ["Name", "Input Values", "Fitness over Time", "Greedy Algorithm Fitness", "Duration of Run"]
+    generalData = []
+    generalData.append(["Name", "Input Values", "Final Fitness", "Greedy Fitness", "Duration of Run"])
+    
+    for test in testResults:
+        name, inputVals, fitOverTime, greedyFit, timeDuration = test
+        generalData.append([name, inputVals, fitOverTime[-1][1], greedyFit, timeDuration])
+        fitnessData.append(fitOverTime)
 
     # Writing to CSV
-    with open(csv_filepath, mode='w', newline='') as file:
+    with open(csvFilepath, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows(testResults)
+        writer.writerows(generalData)
+    
+    csvFilepaths = ["Data/IV/Control Test Results.csv","Data/IV/Low Aca Test Results.csv","Data/IV/Low Ratio Test Results.csv","Data/IV/High Aca Test Results.csv","Data/IV/High Ratio Test Results.csv"]
+
+    for x in range(0, len(csvFilepaths) - 1):
+        with open(csvFilepaths[x], mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Generation", "Fitness"])
+            writer.writerows(fitnessData[x])
 
 Test()
